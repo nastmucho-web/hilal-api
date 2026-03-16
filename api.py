fec7540ac649c1b45c1c55f8d613c4d37fe4ecb9
Ridouane <nastmucho.web@gmail.com>
2026-03-16 09:02:05 +0000

    fix hilal calculation

--- api.py
+++ api.py
@@ -33,26 +33,61 @@ sun = eph["sun"]
 # =========================
 
 countries = {
+
 "Morocco": (34.0209,-6.8416),
 "Algeria": (36.7538,3.0588),
 "Tunisia": (36.8065,10.1815),
 "Libya": (32.8872,13.1913),
 "Egypt": (30.0444,31.2357),
+"Sudan": (15.5007,32.5599),
+"Mauritania": (18.0735,-15.9582),
+
 "Saudi Arabia": (21.3891,39.8579),
 "UAE": (25.2048,55.2708),
 "Qatar": (25.2854,51.5310),
+"Bahrain": (26.2235,50.5876),
 "Kuwait": (29.3759,47.9774),
 "Oman": (23.5880,58.3829),
-"Pakistan": (33.6844,73.0479),
-"Indonesia": (-6.2088,106.8456),
-"Malaysia": (3.1390,101.6869),
+"Yemen": (15.3694,44.1910),
+
+"Jordan": (31.9454,35.9284),
+"Palestine": (31.7683,35.2137),
+"Lebanon": (33.8938,35.5018),
+"Syria": (33.5138,36.2765),
+"Iraq": (33.3152,44.3661),
+
 "Turkey": (41.0082,28.9784),
 "Iran": (35.6892,51.3890),
-"Bangladesh": (23.8103,90.4125),
+
+"Pakistan": (33.6844,73.0479),
 "Afghanistan": (34.5553,69.2075),
+"Bangladesh": (23.8103,90.4125),
+"Maldives": (4.1755,73.5093),
+
+"Indonesia": (-6.2088,106.8456),
+"Malaysia": (3.1390,101.6869),
+"Brunei": (4.9031,114.9398),
+
 "Somalia": (2.0469,45.3182),
-"Sudan": (15.5007,32.5599),
-"Mauritania": (18.0735,-15.9582)
+"Djibouti": (11.8251,42.5903),
+"Eritrea": (15.3229,38.9251),
+"Chad": (12.1348,15.0557),
+"Niger": (13.5116,2.1254),
+"Mali": (12.6392,-8.0029),
+"Senegal": (14.7167,-17.4677),
+"Gambia": (13.4549,-16.5790),
+"Guinea": (9.6412,-13.5784),
+"Sierra Leone": (8.4657,-13.2317),
+"Nigeria": (9.0765,7.3986),
+"Cameroon": (3.8480,11.5021),
+"Uganda": (0.3476,32.5825),
+"Tanzania": (-6.1630,35.7516),
+"Comoros": (-11.7172,43.2473),
+"Mozambique": (-25.9692,32.5732),
+
+"Albania": (41.3275,19.8187),
+"Bosnia": (43.8563,18.4131),
+"Kosovo": (42.6629,21.1655)
 }
 
 days = [
@@ -112,7 +147,7 @@ def find_month(lat,lon,hijri_month):
 
     today = datetime.utcnow().date()
 
-    for i in range(40):
+    for i in range(120):
 
         test_day = today + timedelta(days=i)
 
@@ -131,6 +166,10 @@ def find_month(lat,lon,hijri_month):
                     "gregorian": start.isoformat(),
                     "hijri": f"{h.day} {h.month_name()} {h.year}"
                 }
+
+    return {
+        "error":"month not found"
+    }
 
 # =========================
 # RAMADAN
@@ -176,7 +215,7 @@ def eid_adha_world():
 
         start = find_month(lat,lon,12)
 
-        if start:
+        if "gregorian" in start:
 
             g = datetime.fromisoformat(start["gregorian"]) + timedelta(days=9)
 
@@ -185,8 +224,12 @@ def eid_adha_world():
             results[country] = {
                 "weekday": weekday,
                 "gregorian": g.date().isoformat(),
-                "hijri": f"10 Dhul Hijjah"
+                "hijri": "10 Dhul Hijjah"
             }
+
+        else:
+
+            results[country] = start
 
     return results
 
